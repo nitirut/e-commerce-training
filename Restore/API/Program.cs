73c6a14b9191091581ca99829bf01970c26a335f
Nitirut W. <nitirut.wach@gmail.com>
2025-05-02 20:55:29 +0700

    Update Lesson 26

--- Restore/API/Program.cs
+++ Restore/API/Program.cs
@@ -5,16 +5,23 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("defaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("https://localhost:3000");
+});
+
 app.MapControllers();
 
 app.InitDB();
